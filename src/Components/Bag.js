935f49f4dc9d6212ac1330cd05123d80ec1b7c29
Lok-ii <lokeshkataria.work@gmail.com>
2023-12-26 02:24:27 +0530

    ready

--- src/Components/Bag.js
+++ src/Components/Bag.js
@@ -1,28 +1,84 @@
 import "./bag.css";
 import Item from "./Item";
-import data from "./phoneData.json"
+import data from "./phoneData.json";
+import { useState } from "react";
 
-export default function Bag(){
-    const totalPrice = (price)=>{
-        
-    }
+export default function Bag() {
+  const [totalPrice, setTotalPrice] = useState(2199.96);
+  const [cartItems, setCartItems] = useState(data);
+  const [isCartEmpty, setIsCartEmpty] = useState(cartItems.length === 0);
 
-    return(
-        <div className="container">
-            <h1>Your Bag</h1>
-            <div className="itemList">
-                {
-                    data.map((item, index)=>{
-                        return <Item title={item.title} price={item.price} image={item.image} key={index}/>
-                    })
-                }
-            </div>
-            <div className="totalContainer">
-                <div className="total">
-                    <p className="totalText">Total</p>
-                    <p className="totalPrice"></p>
-                </div>
-            </div>
-        </div>
+  function handleUpdateTotal(title, newCount) {
+    // Update the count for the specific item
+    const updatedCartItems = cartItems.map((item) =>
+      item.title === title ? { ...item, count: newCount } : item
     );
+
+    // Recalculate the total price
+    const newTotalPrice = updatedCartItems.reduce(
+      (total, item) => total + item.price * item.count,
+      0
+    );
+
+    // Update both the cart items and total price
+    setCartItems(updatedCartItems);
+    setTotalPrice(newTotalPrice);
+  }
+
+  function handleRemoveItem(title, count) {
+    // Remove the item from the cartItems state based on the title and count
+    const updatedCartItems = cartItems.filter(
+      (item) => !(item.title === title && item.count === count)
+    );
+    setCartItems(updatedCartItems);
+
+    // Recalculate the total price
+    const newTotalPrice = updatedCartItems.reduce(
+      (total, item) => total + item.price * item.count,
+      0
+    );
+    setTotalPrice(newTotalPrice);
+
+    // Check if the cart is empty
+    setIsCartEmpty(updatedCartItems.length === 0);
+  }
+
+  function clearCart() {
+    // Clear the cart and update total price
+    setCartItems([]);
+    setTotalPrice(0);
+    setIsCartEmpty(true);
+  }
+
+  return (
+    <div className="container">
+      <h1>YOUR BAG</h1>
+      <div className="itemList">
+        {cartItems.map((item, index) => {
+          return (
+            <Item
+              title={item.title}
+              price={item.price}
+              image={item.image}
+              key={index}
+              count={item.count}
+              onRemove={handleRemoveItem}
+              onUpdateTotal={handleUpdateTotal}
+            />
+          );
+        })}
+      </div>
+      <div className="totalContainer">
+        <hr />
+        <div className="total">
+          <p className="totalText">Total</p>
+          <p className="totalPrice">
+            ${" "}
+            {("" + totalPrice).substring(0, ("" + totalPrice).indexOf(".") + 3)}
+          </p>
+        </div>
+        <button className="clearCart" onClick={clearCart}>Clear Cart</button>
+      </div>
+    </div>
+  );
 }
--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,8 +1,44 @@
 import "./item.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Item(props) {
-  let [count, setCount] = useState(1);
+  let [count, setCount] = useState(props.count);
+
+  useEffect(() => {
+    // Update the count when the props.count changes
+    setCount(props.count);
+  }, [props.count]);
+
+  function increaseCount() {
+    setCount(count + 1);
+    props.onUpdateTotal(props.title, count + 1);
+  }
+
+  function decreaseCount() {
+    if (count > 0) {
+        const newCount = count - 1;
+  
+        // If the count reaches 0, remove the item immediately
+        if (newCount === 0) {
+          props.onRemove(props.title, 1);
+        } else {
+          setCount(newCount);
+        }
+      }
+  }
+
+  function removeItem() {
+    // Call the onRemove callback with the item's title and count
+    props.onRemove(props.title, count);
+  }
+
+  // Add a function to update the total price in the parent component
+  function updateTotalPrice(newCount) {
+    // Calculate the new total price based on the updated count
+    const newTotalPrice = newCount * props.price;
+    // Call a callback function to update the total price in the parent component
+    props.onUpdateTotal(newTotalPrice);
+  }
   return (
     <div className="item">
       <div className="phone">
@@ -11,14 +47,14 @@ export default function Item(props) {
         </div>
         <div className="phoneDetails">
           <p className="title">{props.title}</p>
-          <p className="price">{props.price}</p>
-          <p className="remove">remove</p>
+          <p className="price">$ {props.price}</p>
+          <p className="remove" onClick={removeItem}>remove</p>
         </div>
       </div>
       <div className="quantity">
-        <i class="fa-solid fa-angle-up"></i>
+        <i className="fa-solid fa-angle-up" onClick={increaseCount}></i>
         <p className="unit">{count}</p>
-        <i class="fa-solid fa-angle-down"></i>
+        <i className="fa-solid fa-angle-down" onClick={decreaseCount}></i>
       </div>
     </div>
   );
